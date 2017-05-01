d0f8a5138b128ebae33b96afccdde791e8de315d
lightningkay <ew_34@sina.com>
2017-05-01 09:18:16 +0000

    add new

--- pi/base/Logging.h
+++ pi/base/Logging.h
@@ -45,7 +45,7 @@ namespace pi
                 {
                     data_ = slash + 1;
                 }
-                size_ = static_cast<int>(strken(data_));
+                size_ = static_cast<int>(strlen(data_));
             }
 
             const char* data_;
@@ -54,7 +54,7 @@ namespace pi
 
         Logger(SourceFile file, int line);
         Logger(SourceFile file, int line, LogLevel level);
-        Logger(SourceFile file, int line, LogLevel levle, const char* funce);
+        Logger(SourceFile file, int line, LogLevel levle, const char* func);
         Logger(SourceFile file, int line, bool toAbort);
         ~Logger();
 
@@ -86,6 +86,40 @@ namespace pi
 
         Impl impl_;
     };
+
+    extern Logger::LogLevel g_loglevel;
+
+    inline Logger::LogLevel Logger::logLevel()
+    {
+        return g_loglevel;
+    }
+
+#define LOG_TRACE if (pi::Logger::logLevel() <= pi::Logger::TRACE) \
+    pi::Logger(__FILE__, __LINE__, pi::Logger::TRACE, __func__).stream()
+#define LOG_DEBUG if (pi::Logger::logLevel()) <= pi::Logger::DEBUG) \
+    pi::Logger(__FILE__, __LINE__, pi::Logger::TRACE, __func__).stream()
+#define LOG_INFO if(pi::Logger::logLevel() <= pi::Logger::INFO) \
+    pi::Logger(__FILE__, __LINE__, pi::Logger::TRACE, __func__).stream()
+#define LOG_WARN pi::Logger(__FILE__, __LINE__, pi::Logger::WARN).stream()
+#define LOG_ERROR pi::Logger(__FILE__, __LINE__, pi::Logger::ERROR).stream()
+#define LOG_FATAL pi::Logger(__FILE__, __LINE__, pi::Logger::FATAL).stream()
+#define LOG_SYSERR pi::Logger(__FILE__, __LINE__, false).stream()
+#define LOG_SYSFATAL pi::Logger(__FILE__, __LINE__, true).stream()
+
+const char* strerror_tl(int savedErrno);
+
+#define CHECK_NOTNULL(val) \
+    ::pi::CheckNotNull(__FILE__, __LINE__, "'" #val "' Must be non NULL", (val));
+
+template <typename T>
+T* CheckNotNull(Logger::SourceFile file, int line, const char* names, T* ptr)
+{
+    if (ptr == NULL)
+    {
+        Logger(file, line, Logger::FATAL).stream() << names;
+    }
+    return ptr;
+}
 
 }
 #endif
